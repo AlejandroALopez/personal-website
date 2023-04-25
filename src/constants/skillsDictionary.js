764b6823f02e3d5ccf2835bd3ce17c76947c255d
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-04-25 14:19:30 -0400

    skills section remake

---
+++ src/constants/skillsDictionary.js
@@ -0,0 +1,87 @@
+import HTML from '../assets/skills/html.svg';
+import CSS from '../assets/skills/css.svg';
+import JS from '../assets/skills/js.svg';
+import TS from '../assets/skills/ts.svg';
+import React from '../assets/skills/react.svg';
+import Node from '../assets/skills/node.svg';
+import Express from '../assets/skills/express.svg';
+import Git from '../assets/skills/git.svg';
+
+import Mongo from '../assets/skills/mongo.svg';
+import Redux from '../assets/skills/redux.svg';
+import Figma from '../assets/skills/figma.svg';
+import Python from '../assets/skills/python.svg';
+import Angular from '../assets/skills/angular.svg';
+import DevOps from '../assets/skills/devops.svg';
+import Maya from '../assets/skills/maya.svg';
+import Unity from '../assets/skills/unity.svg';
+
+export const skillsMap = [
+    {
+        name: 'HTML',
+        icon: HTML,
+    },
+    {
+        name: 'CSS',
+        icon: CSS,
+    },
+    {
+        name: 'JavaScript',
+        icon: JS,
+    },
+    {
+        name: 'Typescript',
+        icon: TS,
+    },
+    {
+        name: 'React & React Native',
+        icon: React,
+    },
+    {
+        name: 'Node',
+        icon: Node,
+    },
+    {
+        name: 'Express',
+        icon: Express,
+    },
+    {
+        name: 'Git',
+        icon: Git,
+    },
+];
+
+export const skillsExperiencedMap = [
+    {
+        name: 'Mongo',
+        icon: Mongo,
+    },
+    {
+        name: 'Redux',
+        icon: Redux,
+    },
+    {
+        name: 'Figma',
+        icon: Figma,
+    },
+    {
+        name: 'Python',
+        icon: Python,
+    },
+    {
+        name: 'Angular',
+        icon: Angular,
+    },
+    {
+        name: 'Dev Ops',
+        icon: DevOps,
+    },
+    {
+        name: 'Maya',
+        icon: Maya,
+    },
+    {
+        name: 'Unity',
+        icon: Unity,
+    },
+];
--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -1,101 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { skillsMap, skillsExperiencedMap } from '../../constants/skillsDictionary';
+
 import './style.scss';
 
 export default function Skills() {
-  const [tab, setTab] = useState('code');
 
   return (
     <div className="skills-section" id="skills">
-      <p className="title">{tab === 'code' ? 'Code Skills' : 'Soft Skills'}</p>
-      <div className="tab-container">
-        <button type="button" className={tab === 'code' ? 'tab-on' : 'tab-off'} onClick={() => setTab('code')}>
-          <p>Code skills</p>
-        </button>
-        <button type="button" className={tab === 'soft' ? 'tab-on' : 'tab-off'} onClick={() => setTab('soft')}>
-          <p>Soft skills</p>
-        </button>
+      <p className="title">Tools I am skilled at:</p>
+      <div className="skills-container">
+        {skillsMap.map((skill) => {
+          return (
+            <div key={skill.name} className="skill-element">
+              <img src={skill.icon} alt={skill.name}/>
+            </div>
+          );
+        })}
       </div>
-      {tab === 'code'
-        ? (
-          <div className="skills-container">
-            <div>
-              <p className="skill">React Native</p>
-              <div className="bar-background">
-                <div className="bar" id="p90" />
-              </div>
+      <p className="title">Tools I worked with:</p>
+      <div className="skills-container">
+        {skillsExperiencedMap.map((skill) => {
+          return (
+            <div key={skill.name} className="skill-element">
+              <img src={skill.icon} alt={skill.name}/>
             </div>
-            <div>
-              <p className="skill">React</p>
-              <div className="bar-background">
-                <div className="bar" id="p80" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">HTML5 (HTML + CSS + JS)</p>
-              <div className="bar-background">
-                <div className="bar" id="p80" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">NodeJS</p>
-              <div className="bar-background">
-                <div className="bar" id="p70" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Python</p>
-              <div className="bar-background">
-                <div className="bar" id="p60" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Unity</p>
-              <div className="bar-background">
-                <div className="bar" id="p50" />
-              </div>
-            </div>
-          </div>
-        )
-        : (
-          <div className="skills-container">
-            <div>
-              <p className="skill">Communication</p>
-              <div className="bar-background">
-                <div className="bar bar-soft" id="p80" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Teamwork</p>
-              <div className="bar-background">
-                <div className="bar bar-soft" id="p80" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Self-direction</p>
-              <div className="bar-background">
-                <div className="bar bar-soft" id="p70" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Adaptability</p>
-              <div className="bar-background">
-                <div className="bar bar-soft" id="p70" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Problem Solving</p>
-              <div className="bar-background">
-                <div className="bar bar-soft" id="p70" />
-              </div>
-            </div>
-            <div>
-              <p className="skill">Leadership</p>
-              <div className="bar-background">
-                <div className="bar bar-soft" id="p60" />
-              </div>
-            </div>
-          </div>
-        )}
+          );
+        })}
+      </div>
     </div>
   );
 }
